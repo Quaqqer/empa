6fca71c714d4d19d44980fd9691c3f3c5d02972d
Empa <emanuel@empa.xyz>
2022-05-12 20:02:51 +0200

    Make navigation bar sticky

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -2,7 +2,16 @@ import { Box, Flex, Text } from "@chakra-ui/react";
 
 export default function NavigationBar(): JSX.Element {
   return (
-    <Box>
+    <Box
+      width="100%"
+      bg="white"
+      position="sticky"
+      top={0}
+      mb={1}
+      borderBottom={1}
+      borderStyle="solid"
+      borderColor="grey"
+    >
       <Flex p={4}>
         <Text>Hej</Text>
       </Flex>
