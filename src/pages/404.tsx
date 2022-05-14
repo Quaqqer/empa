e321bbea4bbf3078c0d129f8e80a56b5199fefe7
Empa <emanuel@empa.xyz>
2022-05-14 19:40:29 +0200

    Add 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,31 @@
+import { Button, Center, Heading, Text } from "@chakra-ui/react";
+import Head from "next/head";
+import { ReactElement } from "react";
+import NLink from "next/link";
+
+export default function E404(): ReactElement {
+  return (
+    <>
+      <Head>
+        <title>404: Page not found</title>
+      </Head>
+
+      <Center my={10}>
+        <Heading width={700}>Error 404: This page could not be found</Heading>
+      </Center>
+
+      <Center>
+        <Text width={600}>
+          The contents of this page are missing. The page may have been deleted
+          or it never existed to begin with.
+        </Text>
+      </Center>
+
+      <Center>
+        <NLink href="/" passHref>
+          <Button m={10}>Go back to home</Button>
+        </NLink>
+      </Center>
+    </>
+  );
+}
