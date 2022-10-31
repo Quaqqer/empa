bd4487834866551ff60bd171344faa08e19220dd
Empa <emanuel@empa.xyz>
2022-10-31 23:02:06 +0100

    Fix eol

--- frontend/src/games/gol/theme.ts
+++ frontend/src/games/gol/theme.ts
@@ -9,13 +9,12 @@ export const darkTheme: Theme = {
   cell: "#a3be8c",
   background: "#2e3440",
   borders: "#434c5e",
-  hover: "#80a3be8c"
+  hover: "#80a3be8c",
 };
 
 export const lightTheme: Theme = {
   cell: "#a3be8c",
   background: "#eceff4",
   borders: "#d8dee9",
-  hover: "#80a3be8c"
+  hover: "#80a3be8c",
 };
-
